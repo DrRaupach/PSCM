a5d7ce71258b8b6fcf7763b3d611fce4861c9236
Dr. Rainer Raupach <rainer.raupach@gmx.net>
2026-09-06 11:24:09 +0200

    package renaming and update updates.xri

--- sha1hashcode.ps1
+++ sha1hashcode.ps1
@@ -1,2 +1,2 @@
-(Get-FileHash -Path "D:\_User\Rainer\Documents\_Software\GitHub\PSCM\PSCM.zip" -Algorithm SHA1).Hash.ToLower()
+(Get-FileHash -Path "D:\_User\Rainer\Documents\_Software\GitHub\PSCM\PSCM_v8engine_v130.zip" -Algorithm SHA1).Hash.ToLower()
 Pause
